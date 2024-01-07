7563f4723b6f7160fe3df01e6d6163db020bd2e0
plasticine9750 <975036719@qq.com>
2024-01-07 13:55:01 +0800

    feat: add manualChunks for code-splitting demos

--- demos/code-splitting-after-2-9/vite.config.js
+++ demos/code-splitting-after-2-9/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'react-router-vendor': ['react-router-dom'],
+          'utilities-library-vendor': ['localforage', 'match-sorter', 'match-sorter'],
+        },
+      },
+    },
+  },
 })
